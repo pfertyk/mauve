c107ef8a720dc034bc46fbb694b9e53d5a806d07
Paweł Fertyk <pfertyk@openmailbox.org>
2017-07-19 09:24:23 +0200

    Simplify tests

--- spec/renderer.spec.js
+++ spec/renderer.spec.js
@@ -12,33 +12,33 @@ describe ('Renderer', () => {
 
   it('converts a markdown file', (done) => {
     const markdownFilePath = path.join(__dirname, 'md/header.md')
-    const expectedHtml = '^[^<]*<div class="markdown-body"><h1 id="hello">Hello</h1></div>$'
 
     renderer.loadFile(markdownFilePath).then(() => {
-      expect(callback.calls.count()).toEqual(1)
-      expect(decodeURIComponent(callback.calls.mostRecent().args[0])).toMatch(expectedHtml)
+      expect(callback).toHaveBeenCalledWith(
+        '<div class="markdown-body"><h1 id="hello">Hello</h1></div>'
+      )
       done()
     }, done.fail)
   })
 
   it('converts a markdown string', (done) => {
     var markdown = '## Hi'
-    const expectedHtml = '^[^<]*<div class="markdown-body"><h2 id="hi">Hi</h2></div>$'
 
     renderer.load(markdown).then(() => {
-      expect(callback.calls.count()).toEqual(1)
-      expect(decodeURIComponent(callback.calls.mostRecent().args[0])).toMatch(expectedHtml)
+      expect(callback).toHaveBeenCalledWith(
+        '<div class="markdown-body"><h2 id="hi">Hi</h2></div>'
+      )
       done()
     }, done.fail)
   })
 
   it('preserves new lines in code', (done) => {
     const markdownFilePath = path.join(__dirname, 'md/code.md')
-    const expectedHtml = '^[^<]*<div class="markdown-body"><pre><code>var x = 3\nvar y = null\n</code></pre></div>'
 
     renderer.loadFile(markdownFilePath).then(() => {
-      expect(callback.calls.count()).toEqual(1)
-      expect(decodeURIComponent(callback.calls.mostRecent().args[0])).toMatch(expectedHtml)
+      expect(callback).toHaveBeenCalledWith(
+        '<div class="markdown-body"><pre><code>var x = 3\nvar y = null\n</code></pre></div>'
+      )
       done()
     }, done.fail)
   })
