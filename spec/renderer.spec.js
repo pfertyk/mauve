96545a874bdbd66113747b1177a1ff0887c6389e
Paweł Fertyk <pfertyk@openmailbox.org>
2017-07-19 08:29:15 +0200

    Decode html in tests

--- spec/renderer.spec.js
+++ spec/renderer.spec.js
@@ -11,22 +11,23 @@ describe ('Renderer', () => {
 
   it('converts a markdown file', (done) => {
     const markdownFilePath = __dirname + '/test_readme.md'
-    const expectedHTML = 'data:text/html;charset=UTF-8,%3Cdiv%20class%3D%22markdown-body%22%3E%3Ch1%20id%3D%22hello%22%3EHello%3C%2Fh1%3E%3C%2Fdiv%3E'
+    const expectedHtml = '^[^<]*<div class="markdown-body"><h1 id="hello">Hello</h1></div>$'
+
 
     renderer.loadFile(markdownFilePath).then(() => {
       expect(callback.calls.count()).toEqual(1)
-      expect(callback.calls.mostRecent().args[0]).toEqual(expectedHTML)
+      expect(decodeURIComponent(callback.calls.mostRecent().args[0])).toMatch(expectedHtml)
       done()
     }, done.fail)
   })
 
   it('converts a markdown string', (done) => {
     var markdown = '## Hi'
-    const expectedHTML = 'data:text/html;charset=UTF-8,%3Cdiv%20class%3D%22markdown-body%22%3E%3Ch2%20id%3D%22hi%22%3EHi%3C%2Fh2%3E%3C%2Fdiv%3E'
+    const expectedHtml = '^[^<]*<div class="markdown-body"><h2 id="hi">Hi</h2></div>$'
 
     renderer.load(markdown).then(() => {
       expect(callback.calls.count()).toEqual(1)
-      expect(callback.calls.mostRecent().args[0]).toEqual(expectedHTML)
+      expect(decodeURIComponent(callback.calls.mostRecent().args[0])).toMatch(expectedHtml)
       done()
     }, done.fail)
   })
